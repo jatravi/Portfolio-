1af0039eb3dcdbdfd9f0a2cc3755bb5137a0e301
Ravi Choudhary <01jat.ravi@gmail.com>
2025-05-19 14:10:18 +0530

    Update Home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,40 @@
-import '../styles/Home.css'
-import thinkingBubble from '../assets/thinking-bubble.png';
-import 'boxicons/css/boxicons.min.css';
+import "../styles/Home.css";
+import thinkingBubble from "../assets/thinking-bubble.png";
+import "boxicons/css/boxicons.min.css";
 
-function Home(){
-    return(
-        <div id="home">
-            <div className="info">
-               <div className="info-title">
-                 <i className='bx bxl-sketch'></i> 
-                 Front-end Developer Portfolio
-               </div>
-               <h1>Providing <span className="gradient">the best</span> Project <span className="gradient">Experience</span></h1>
-               <p>I'm a Front-end developer with Experience in website, Mobile and Software Development. Check out my skills and projects. </p>
-               <button><i className='bx bxs-send'> Contact Me</i></button>
-            </div>
-            <div className="info-box">
-                <img src={thinkingBubble} alt="Thinking Bubble" />
-            </div>
-            <div className="scroll-down">
-
-            </div>
+function Home() {
+  return (
+    <div id="home">
+      <div className="info">
+        <div className="info-title">
+          <i className="bx bxl-sketch"> Front-End Developer Portfolio</i>
         </div>
-    )
-};
+
+        <h1>
+          Delivering <span className="gradient">Exceptional</span> Project
+          <span className="gradient"> Experiences</span>
+        </h1>
+
+        <p>
+          I'm a front-end developer with hands-on experience in web, mobile, and
+          software development. Explore my portfolio to see how I bring ideas to
+          life through clean code, engaging UIs, and user-centric design.
+        </p>
+
+        <button>
+          <i className="bx bxs-send"> Contact Me</i>
+        </button>
+      </div>
+
+      <div className="info-box">
+        <img src={thinkingBubble} alt="Thinking Bubble" />
+      </div>
+
+      <div className="scroll-down">
+       
+      </div>
+    </div>
+  );
+}
 
 export default Home;
